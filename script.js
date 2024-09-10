8740905952cef3d4822388c9a23ba2d98c1ebcd6
banpogi <banbansoy1@gmail.com>
2024-09-11 00:37:10 +0800

    added UI and implement Javascript Dom so user can play by clicking buttons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello UI BRANCH")
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -11,7 +11,7 @@ function getComputerChoice(){
     } else if (computerChoice == 2) {
         return "paper"
     } else {
-        return "scissor"
+        return "scissor" 
     }
   
 }
@@ -19,23 +19,23 @@ function getComputerChoice(){
 
 
 
-function getHumanChoice(){
+// function getHumanChoice(){
 
-    let humanChoice = prompt("Enter rock, paper, or scissor").toLowerCase();
-    if(humanChoice == "rock"){
+//     let humanChoice = prompt("Enter rock, paper, or scissor").toLowerCase();
+//     if(humanChoice == "rock"){
         
-        return "rock"
+//         return "rock"
         
-    } else if (humanChoice == "paper"){
-        return "paper"
-    } else if ( humanChoice == "scissor") {
-        return "scissor"
-    } else {
-        return "invalid"
-    }
+//     } else if (humanChoice == "paper"){
+//         return "paper"
+//     } else if ( humanChoice == "scissor") {
+//         return "scissor"
+//     } else {
+//         return "invalid"
+//     }
 
 
-}
+// }
 
 
 
@@ -44,33 +44,110 @@ function playround(humanChoice, computerChoice) {
     
 
     if(humanChoice===computerChoice){
-        console.log("ITS A TIE")
+        console.log("tie")
+        return "It's a tie"
     }  else if(humanChoice == "rock" && computerChoice == "scissor" || humanChoice =="paper" && computerChoice == "rock" || humanChoice =="scissor" && computerChoice=="paper"){
-    humanScore++
+        humanScore++
+        console.log("I win")   
+    return "Player Win"
     } else {
-    computerScore++; }
+        computerScore++
+        console.log("Cpu win")       
+    return "Computer Win" }
 
-  console.log("My Score " +""+ humanScore + "" + "" +" Computer Score " + computerScore)
 
 }
+
+
+const score = document.querySelector(".score");
+const result = document.querySelector(".result");
+const outcome = document.querySelector(".outcome")
+const btn = document.querySelector(".container")
+
+
+document.querySelector
 
 
 
 
 function playGame(){
-    for(let i =1 ; i <= 5; i++){
-        console.log(`ROUND ${i}`)
-
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playround(humanSelection, computerSelection);
-
-        console.log("I choose " + humanSelection)
-        console.log("Computer choose " + computerSelection)
 
 
+    const buttonRock = document.querySelector(".button-one")
+    const buttonPaper = document.querySelector(".button-two")
+    const buttonScissor  = document.querySelector(".button-three")
+
+    function handleClick(e) {
+         // If buttons are disabled, stop the function execution
+        if (buttonRock.disabled || buttonPaper.disabled || buttonScissor.disabled) {
+            return;
+        }
+
+        let playerSelect = e.target.id;     
+        let computerSelect = getComputerChoice()
+        console.log(e.target.id, computerSelect)
+       
+        
+        let gameResult =  playround(playerSelect, computerSelect)
+        result.textContent = `You choose ${playerSelect} and CPU choose ${computerSelect} so ${gameResult}`
+        score.textContent = `Player Score ${humanScore}  CPU Score ${computerScore}`
+
+        if(humanScore === 5){
+            outcome.textContent = "YOU WIN!!!"
+            buttonRock.disabled = true;
+            buttonPaper.disabled = true;
+            buttonScissor.disabled = true;
+            
+        } else if (computerScore===5){
+            outcome.textContent = "CPU WIN"
+           
+            buttonRock.disabled = true;
+            buttonPaper.disabled = true;
+            buttonScissor.disabled = true;
+        }
+
+        
     }
 
+ 
+    buttonRock.addEventListener('click', handleClick);
+    buttonPaper.addEventListener('click', handleClick);
+    buttonScissor.addEventListener('click', handleClick);
+    
+    
+    
 }
 
+
+
+
 playGame()
+
+
+
+
+
+
+
+
+
+
+
+
+// function playGame(){
+//     for(let i =1 ; i <= 5; i++){
+//         console.log(`ROUND ${i}`)
+
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         playround(humanSelection, computerSelection);
+
+//         console.log("I choose " + humanSelection)
+//         console.log("Computer choose " + computerSelection)
+
+
+//     }
+
+// }
+
+// playGame()
